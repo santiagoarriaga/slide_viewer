67ddc5bf452eccad451728bcc66b639604c3bcaa
santiago <santiago@nul-unu.com>
2017-10-09 17:08:42 -0500

    refactor and document

--- src/sagas.js
+++ src/sagas.js
@@ -1,23 +1,23 @@
 import { takeLatest } from 'redux-saga'
 import { call, fork, put } from 'redux-saga/effects'
 
-class Modules
-{
-  static load()
-  {
-    return Promise.resolve({ curso: 1 })
-  }
-}
-
+/**
+ * Show the selected module
+ */
 function* moduleShow( action )
 {
-  const module = yield call( Modules.load )
+  const module = yield call( () =>
+    { return Promise.resolve({ curso: 1 }) }
+  )
 
   const event = { type: 'module.show', module }
 
   yield put( event )
 }
 
+/**
+ * Main saga generator.
+ */
 export function* sagas()
 {
   yield [
